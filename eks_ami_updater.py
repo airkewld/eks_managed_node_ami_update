5d1f619878a199c0f4073ae917bc9ce15eccdfc3
Oscar Rodriguez <oscar@macbook-pro.lan>
2022-09-23 16:57:50 -0400

    better logging and logic to run locally or serverless

--- eks_ami_updater.py
+++ eks_ami_updater.py
@@ -1,11 +1,29 @@
 import boto3,os,logging
 
-try:
-  AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-  AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-  AWS_DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
-except KeyError:
-  logging.warning("Missing optional environment variable")
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT,level=logging.INFO)
+
+homeDir = os.getenv("HOME")
+
+if os.path.exists(homeDir + "/.aws/credentials") == True:
+  logging.info("Using local credentials file...")
+elif os.path.exists(homeDir + "/.aws/credentials") != True:
+  logging.info("Credentials file not found.")
+  try:
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+  except KeyError:
+    logging.error("Missing container environment variable AWS_ACCESS_KEY_ID.")
+    exit(1)
+  try:
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+  except KeyError:
+    logging.error("Missing container environment variable AWS_SECRET_ACCESS_KEY.")
+    exit(1)
+  try:
+    AWS_DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
+  except KeyError:
+    logging.error("Missing container environment variable AWS_DEFAULT_REGION." )
+    exit(1)
 
 try:
   CLUSTER = os.environ['CLUSTER_NAME']
@@ -20,6 +38,7 @@ try:
       group_status = description["nodegroup"]
       status = group_status["status"]
       if status == "ACTIVE":
+        logging.info(group_status["nodegroupName"] + " is " + status)
         update_vendor_registry = eks_client.update_nodegroup_version(clusterName=CLUSTER,nodegroupName=group,force=True)
       description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
       group_status = description["nodegroup"]
@@ -29,7 +48,7 @@ try:
         description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
         group_status = description["nodegroup"]
         status = group_status["status"]
-        print(group_status["nodegroupName"] + " is " + status)
+        logging.info(group_status["nodegroupName"] + " is " + status)
         status = group_status["status"]
 
   ## Loop tough each node and output its status
@@ -37,8 +56,9 @@ try:
       description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
       group_status = description["nodegroup"]
       status = group_status["status"]
-      print(group_status["nodegroupName"] + " is " + status)
+      logging.info(group_status["nodegroupName"] + " is " + status)
 except KeyError:
     logging.error("Missing required CLUSTER_NAME environment variable")
+    exit(1)
 except:
     logging.error("Cluster name did not match environment credentials.")
