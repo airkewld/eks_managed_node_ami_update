a504459050423a89a896621b9b909c11fb921ac7
Oscar Rodriguez <oscar@macbook-pro.lan>
2022-09-23 13:18:46 -0400

    catch missing optional and required env variables

--- eks_ami_updater.py
+++ eks_ami_updater.py
@@ -1,36 +1,42 @@
-import boto3,os
+import boto3,os,logging
 
-CLUSTER = os.environ['CLUSTER_NAME']
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
+try:
+  AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+  AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+  AWS_DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
+except KeyError:
+  logging.warning("Missing optional environment variable")
 
-eks_client = boto3.client('eks')
+try:
+  CLUSTER = os.environ['CLUSTER_NAME']
+  eks_client = boto3.client('eks')
 
-node_group = eks_client.list_nodegroups(clusterName=CLUSTER)
-nodegroups = node_group["nodegroups"]
+  node_group = eks_client.list_nodegroups(clusterName=CLUSTER)
+  nodegroups = node_group["nodegroups"]
 
-## Loop tough the nodegroups of the eks cluster and upgrade one at a time while outputting the status of the upgrade
-for group in nodegroups:
-    description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
-    group_status = description["nodegroup"]
-    status = group_status["status"]
-    if status == "ACTIVE":
-      update_vendor_registry = eks_client.update_nodegroup_version(clusterName=CLUSTER,nodegroupName=group,force=True)
-    description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
-    group_status = description["nodegroup"]
-    status = group_status["status"]
-    while status != "ACTIVE":
-      # update_vendor_registry = eks_client.update_nodegroup_version(clusterName=CLUSTER,nodegroupName=group,force=True)
+  ## Loop tough the nodegroups of the eks cluster and upgrade one at a time while outputting the status of the upgrade
+  for group in nodegroups:
+      description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
+      group_status = description["nodegroup"]
+      status = group_status["status"]
+      if status == "ACTIVE":
+        update_vendor_registry = eks_client.update_nodegroup_version(clusterName=CLUSTER,nodegroupName=group,force=True)
+      description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
+      group_status = description["nodegroup"]
+      status = group_status["status"]
+      while status != "ACTIVE":
+        # update_vendor_registry = eks_client.update_nodegroup_version(clusterName=CLUSTER,nodegroupName=group,force=True)
+        description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
+        group_status = description["nodegroup"]
+        status = group_status["status"]
+        print(group_status["nodegroupName"] + " is " + status)
+        status = group_status["status"]
+
+  ## Loop tough each node and output its status
+  for group in nodegroups:
       description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
       group_status = description["nodegroup"]
       status = group_status["status"]
       print(group_status["nodegroupName"] + " is " + status)
-      status = group_status["status"]
-
-## Loop tough each node and output its status
-for group in nodegroups:
-    description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
-    group_status = description["nodegroup"]
-    status = group_status["status"]
-    print(group_status["nodegroupName"] + " is " + status)
+except KeyError:
+    logging.error("Missing required CLUSTER_NAME environment variable")
