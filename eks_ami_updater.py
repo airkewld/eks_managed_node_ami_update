5a843682e4c51bb2aee958beab0684f66276c0bd
Oscar Rodriguez <oscar@macbook-pro.lan>
2022-09-26 11:53:18 -0400

    check that all required env vars are set and add additional error handling

--- eks_ami_updater.py
+++ eks_ami_updater.py
@@ -1,64 +1,65 @@
-import boto3,os,logging
+import boto3
+import botocore
+import os
+import logging
 
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT,level=logging.INFO)
 
-homeDir = os.getenv("HOME")
-
-if os.path.exists(homeDir + "/.aws/credentials") == True:
-  logging.info("Using local credentials file...")
-elif os.path.exists(homeDir + "/.aws/credentials") != True:
-  logging.info("Credentials file not found.")
-  try:
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-  except KeyError:
-    logging.error("Missing container environment variable AWS_ACCESS_KEY_ID.")
-    exit(1)
-  try:
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-  except KeyError:
-    logging.error("Missing container environment variable AWS_SECRET_ACCESS_KEY.")
-    exit(1)
-  try:
-    AWS_DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
-  except KeyError:
-    logging.error("Missing container environment variable AWS_DEFAULT_REGION." )
-    exit(1)
-
+## Ensure that credentials are set.
+try:
+  AWS_DEFAULT_REGION = os.environ['AWS_DEFAULT_REGION']
+except KeyError:
+  logging.error("Missing AWS_DEFAULT_REGION environment variable")
+  exit(1)
+try:
+  AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+except KeyError:
+  logging.error("Partial credentials found in env, missing: AWS_ACCESS_KEY_ID")
+  exit(1)
+try:
+  AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+except KeyError:
+  logging.error("Partial credentials found in env, missing: AWS_SECRET_ACCESS_KEY")
+  exit(1)
 try:
   CLUSTER = os.environ['CLUSTER_NAME']
-  eks_client = boto3.client('eks')
+except KeyError:
+    logging.error("Missing required CLUSTER_NAME environment variable.")
+    exit(1)
 
-  node_group = eks_client.list_nodegroups(clusterName=CLUSTER)
-  nodegroups = node_group["nodegroups"]
+## set client variable
+eks_client = boto3.client('eks')
+
+try:
+  nodegroups = eks_client.list_nodegroups(clusterName=CLUSTER)["nodegroups"]
 
   ## Loop tough the nodegroups of the eks cluster and upgrade one at a time while outputting the status of the upgrade
   for group in nodegroups:
-      description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
-      group_status = description["nodegroup"]
-      status = group_status["status"]
+      status = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)["nodegroup"]["status"]
       if status == "ACTIVE":
-        logging.info(group_status["nodegroupName"] + " is " + status)
-        update_vendor_registry = eks_client.update_nodegroup_version(clusterName=CLUSTER,nodegroupName=group,force=True)
-      description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
-      group_status = description["nodegroup"]
-      status = group_status["status"]
-      while status != "ACTIVE":
+        logging.info(group + " is " + status)
         # update_vendor_registry = eks_client.update_nodegroup_version(clusterName=CLUSTER,nodegroupName=group,force=True)
-        description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
-        group_status = description["nodegroup"]
-        status = group_status["status"]
-        logging.info(group_status["nodegroupName"] + " is " + status)
-        status = group_status["status"]
+      status = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)["nodegroup"]["status"]
+      while status != "ACTIVE":
+        status = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)["nodegroup"]["status"]
+        logging.info(group + " is " + status)
+        status = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)["nodegroup"]["status"]
 
   ## Loop tough each node and output its status
   for group in nodegroups:
-      description = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)
-      group_status = description["nodegroup"]
-      status = group_status["status"]
-      logging.info(group_status["nodegroupName"] + " is " + status)
-except KeyError:
-    logging.error("Missing required CLUSTER_NAME environment variable")
+      status = eks_client.describe_nodegroup(clusterName=CLUSTER,nodegroupName=group)["nodegroup"]["status"]
+      logging.info(group + " is " + status)
+
+except botocore.exceptions.NoCredentialsError:
+    logging.error("Please provide credentials via environment variables.")
     exit(1)
-except:
-    logging.error("Cluster name did not match environment credentials.")
+except eks_client.exceptions.ResourceNotFoundException:
+    logging.error(CLUSTER + " eks cluster not found.")
+    exit(1)
+except eks_client.exceptions.ClientError:
+    logging.error("Bad credentials provided. Try again.")
+    exit(1)
+except botocore.exceptions.EndpointConnectionError:
+    print(AWS_DEFAULT_REGION)
+    logging.error("Could not connect to the endpoint URL: https://eks." + AWS_DEFAULT_REGION + ".amazonaws.com/clusters/" + CLUSTER + "/node-groups")
